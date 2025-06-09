427698c4f56bfca237fbf86e5863508a29ea0d78
pdamasceno <pedro.damasceno@hotmail.com>
2025-06-09 18:09:33 +0200

    update leet

---
+++ leetcode/128-longest-consecutive-sequence.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
+
+from collections import Counter
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        sortedNums = sorted(set(nums))
+
+        max_consecutive_length = 1
+        length = 1
+        for i in range(len(sortedNums)):
+            if i == 0:
+                continue
+            if sortedNums[i] - sortedNums[i-1] == 1:
+                length += 1
+            else:
+                length = 1
+
+            max_consecutive_length = max(max_consecutive_length, length)
+        return max_consecutive_length
+            
+longestConsecutive = Solution().longestConsecutive([100, 4, 200, 1, 3, 2])
---
+++ leetcode/242-valid-anagram.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/valid-anagram/
+
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        CounterS = Counter(s)
+        CounterT = Counter(t)
+
+        return True if CounterS == CounterT else False
